18b763798c742fceac97308d7fa4ef60bcf338d6
ZenonHh <hienkun07@gmai.com>
2026-03-25 13:52:18 +0700

    Lưu tạm trước khi lấy code mới

---
+++ ViewModels/MapViewModel.cs
@@ -0,0 +1,11 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace DoAnCSharp.ViewModels;
+
+public partial class MapViewModel : ObservableObject
+{
+    public MapViewModel()
+    {
+        // Sau này bạn có thể thêm logic xử lý tọa độ bản đồ ở đây
+    }
+}
